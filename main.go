63c83f43e38f8af8d427bf773d0e6a2202a2f40b
sarunas <sbucius@exadel.com>
2020-11-17 08:59:32 +0200

    run app until exit signal

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var version string
 
 func main() {
+	sig := make(chan os.Signal)
+	signal.Notify(sig,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+	)
+
 	fmt.Println("Hello")
 	fmt.Println(version)
+
+	log.Println(<-sig)
+	os.Exit(0)
 }
