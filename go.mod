e5893ec6a4e0403b76eb178c8f8c81d349654133
sarunas <sbucius@exadel.com>
2020-11-23 13:08:53 +0200

    add kafka and cassandra

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/SarunasBucius/kafka-cass-practise
+
+go 1.15
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.5.2
+	github.com/gocql/gocql v0.0.0-20201024154641-5913df4d474e
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/gocql/gocql"
 )
 
 var version string
@@ -17,8 +20,54 @@ func main() {
 		syscall.SIGQUIT,
 	)
 
+	prod := kafkaProducerConn()
+	defer prod.Close()
+	cons := kafkaConsumerConn()
+	defer cons.Close()
+	db := cassConn()
+	defer db.Close()
+
+	checkKafkaConn(cons)
+
 	fmt.Println("Hello")
 	fmt.Println(version)
 
 	log.Println(<-sig)
+}
+
+func cassConn() *gocql.Session {
+	cluster := gocql.NewCluster(os.Getenv("CASSANDRA_HOST"))
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		panic(err)
+	}
+	return session
+}
+
+func kafkaConsumerConn() *kafka.Consumer {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KAFKA_HOST"),
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
+
+func kafkaProducerConn() *kafka.Producer {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": os.Getenv("KAFKA_HOST")})
+	if err != nil {
+		panic(err)
+	}
+	return p
+}
+
+func checkKafkaConn(c *kafka.Consumer) {
+	_, err := c.GetMetadata(nil, false, 10000)
+	if err != nil {
+		panic(err)
+	}
 }
