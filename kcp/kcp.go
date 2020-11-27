c0d7f1aff7f311dc793105cbe082917714b0f507
sarunas <sbucius@exadel.com>
2020-11-27 15:27:56 +0200

    update kcp package

--- kcp/kcp.go
+++ kcp/kcp.go
@@ -2,30 +2,16 @@
 // and data insertion.
 package kcp
 
-import "errors"
-
 // Kcp contains Producer, Handler and Insert
 type Kcp struct {
-	*Produce
-	*Handle
-	*Insert
+	Producer
+	Handler
+	Inserter
 }
 
 // New takes Producer, Handler, Inserter and returns Kcp instance and an error
-func New(p Producer, h Handler, i Inserter) (*Kcp, error) {
-	if p == nil || h == nil || i == nil {
-		return nil, errors.New("missing implementations")
-	}
-	return &Kcp{
-		&Produce{p},
-		&Handle{h},
-		&Insert{i},
-	}, nil
-}
-
-// Produce contains Producer interface.
-type Produce struct {
-	Producer
+func New(p Producer, h Handler, i Inserter) *Kcp {
+	return &Kcp{Producer: p, Handler: h, Inserter: i}
 }
 
 // Producer produces event.
@@ -33,23 +19,13 @@ type Producer interface {
 	ProduceEvent() error
 }
 
-// NewProduce takes Producer as param and creates new Produce instance
-func NewProduce(p Producer) *Produce {
-	return &Produce{Producer: p}
-}
-
 // ProduceVisit produces visit and returns error.
-func (p *Produce) ProduceVisit() error {
-	return p.ProduceEvent()
+func (k *Kcp) ProduceVisit() error {
+	return k.ProduceEvent()
 }
 
 // Event represets string value of event.
 type Event string
-
-// Handle contains Handler interface.
-type Handle struct {
-	Handler
-}
 
 // Handler interface handles event.
 type Handler interface {
@@ -57,13 +33,8 @@ type Handler interface {
 }
 
 // HandleVisit handles visit event and returns error.
-func (c *Handle) HandleVisit(event Event) error {
-	return c.HandleEvent(event)
-}
-
-// Insert contains Inserter interface.
-type Insert struct {
-	Inserter
+func (k *Kcp) HandleVisit(event Event) error {
+	return k.HandleEvent(event)
 }
 
 // Inserter interface inserts event.
@@ -72,6 +43,6 @@ type Inserter interface {
 }
 
 // InsertVisit inserts visit event and returns error.
-func (i *Insert) InsertVisit(event Event) error {
-	return i.InsertEvent(event)
+func (k *Kcp) InsertVisit(event Event) error {
+	return k.InsertEvent(event)
 }
