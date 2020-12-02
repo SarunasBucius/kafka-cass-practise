21bb33c9e3961d24559734336462c8808051d9ef
sarunas <sbucius@exadel.com>
2020-12-02 11:18:01 +0200

    remove unnecessary functions

--- cmd/kcp/main.go
+++ cmd/kcp/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -53,11 +51,6 @@ func runApp() error {
 		return err
 	}
 	defer db.Close()
-
-	err = checkKafkaConn(cons)
-	if err != nil {
-		return err
-	}
 
 	k := kcp.New(
 		&async.Produce{Producer: prod},
@@ -128,56 +121,4 @@ func kafkaProducerConn() (*kafka.Producer, error) {
 		return nil, err
 	}
 	return p, nil
-}
-
-func checkKafkaConn(c *kafka.Consumer) error {
-	fmt.Println("Checking initial connection")
-	md, err := c.GetMetadata(nil, false, 10000)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	fmt.Printf("Originating broker: %v\n", md.OriginatingBroker)
-	fmt.Printf("Brokers: %v\n", md.Brokers)
-	h, err := net.LookupHost(md.Brokers[0].Host)
-	if err != nil {
-		return err
-	} else if len(h) < 1 {
-		return errors.New("host address not found")
-	}
-	fmt.Println(h)
-	if _, ok := md.Topics["testTopic"]; !ok {
-		err = insertTopic(c)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func insertTopic(c *kafka.Consumer) error {
-	fmt.Println("Creating new topic")
-	a, err := kafka.NewAdminClientFromConsumer(c)
-	if err != nil {
-		fmt.Printf("Failed to create Admin client: %s\n", err)
-		return err
-	}
-
-	defer a.Close()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	_, err = a.CreateTopics(
-		ctx,
-		[]kafka.TopicSpecification{{
-			Topic:             "testTopic",
-			NumPartitions:     1,
-			ReplicationFactor: 1}},
-		kafka.SetAdminOperationTimeout(time.Second*15))
-	if err != nil {
-		fmt.Printf("Failed to create topic: %v\n", err)
-		return err
-	}
-	return nil
-
 }
