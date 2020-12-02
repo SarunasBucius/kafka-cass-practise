fde178006c0b2516b700ca8af56235e5de5225e3
sarunas <sbucius@exadel.com>
2020-12-02 10:50:34 +0200

    implement func ProduceEvent

--- platform/async/async.go
+++ platform/async/async.go
@@ -1,6 +1,9 @@
 package async
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 
 	"github.com/SarunasBucius/kafka-cass-practise/kcp"
@@ -13,6 +16,21 @@ type Produce struct {
 
 // ProduceEvent produces kcp.Event to kafka
 func (p *Produce) ProduceEvent(event kcp.Event) error {
+	topic := "visits"
+	if err := p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(time.Time(event).String()),
+	}, nil); err != nil {
+		fmt.Printf("Produce failed: %v\n", err)
+		return err
+	}
+
+	e := <-p.Events()
+	m := e.(*kafka.Message)
+	if m.TopicPartition.Error != nil {
+		fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+		return m.TopicPartition.Error
+	}
 	return nil
 }
 
