83c88167e658777943e2a1d926ba5c070b34868b
sarunas <sbucius@exadel.com>
2020-12-11 08:40:38 +0200

    add Handler interface to http server

--- platform/services/handler.go
+++ platform/services/handler.go
@@ -8,17 +8,20 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-
-	"github.com/SarunasBucius/kafka-cass-practise/kcp"
 )
 
-// ListenHTTP listens and serves http requests
-func ListenHTTP(ctx context.Context, k *kcp.Kcp, cancel context.CancelFunc, wg *sync.WaitGroup) {
+// Handler contains methods to handle request.
+type Handler interface {
+	ProduceVisit(ip string) error
+}
+
+// ListenHTTP listens and serves http requests.
+func ListenHTTP(ctx context.Context, h Handler, cancel context.CancelFunc, wg *sync.WaitGroup) {
 	srv := &http.Server{
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
-		Handler:      SetRoutes(k),
+		Handler:      SetRoutes(h),
 	}
 	go func() {
 		defer wg.Done()
@@ -30,17 +33,17 @@ func ListenHTTP(ctx context.Context, k *kcp.Kcp, cancel context.CancelFunc, wg *
 	}
 }
 
-// SetRoutes sets routes for http.ListenAndServe
-func SetRoutes(k *kcp.Kcp) *mux.Router {
+// SetRoutes sets routes for http.ListenAndServe.
+func SetRoutes(h Handler) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/visited", visitedHandler(k))
+	r.HandleFunc("/api/visited", visitedHandler(h))
 	return r
 }
 
-func visitedHandler(k *kcp.Kcp) func(w http.ResponseWriter, r *http.Request) {
+func visitedHandler(h Handler) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ip := strings.Split(r.RemoteAddr, ":")[0]
-		if err := k.ProduceVisit(ip); err != nil {
+		if err := h.ProduceVisit(ip); err != nil {
 			http.Error(w, "unexpected error occured", http.StatusInternalServerError)
 			return
 		}
