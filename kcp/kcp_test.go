aeecaeb75c8ea74dc2bd235055a6fafb8a9ba251
sarunas <sbucius@exadel.com>
2020-12-21 16:02:46 +0200

    update TestProduceVisit
    update expected parameter evaluation

--- kcp/kcp_test.go
+++ kcp/kcp_test.go
@@ -1,7 +1,9 @@
 package kcp_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 
@@ -17,14 +19,42 @@ func TestProduceVisit(t *testing.T) {
 	k := kcp.New(mockProdEvent, nil)
 
 	param := "ip"
-	mockProdEvent.EXPECT().ProduceEvent(gomock.Any()).
+	mockProdEvent.EXPECT().
+		ProduceEvent(approxTime{dev: time.Second, ip: param}).
 		Return(nil).
-		Times(1).
-		Do(func(arg kcp.Event) {
-			if param != arg.IP {
-				t.Fail()
-			}
-		})
-	k.ProduceVisit(param)
+		Times(1)
 
+	k.ProduceVisit(param)
+}
+
+type approxTime struct {
+	dev time.Duration
+	ip  string
+}
+
+func (a approxTime) Matches(event interface{}) bool {
+	ev, ok := event.(kcp.Event)
+	if !ok {
+		return false
+	}
+
+	now := time.Now().UTC()
+	if now.Sub(ev.VisitedAt) > a.dev || now.Sub(ev.VisitedAt) < 0 {
+		return false
+	}
+
+	if ev.VisitedAt.Weekday().String() != now.Weekday().String() {
+		return false
+	}
+
+	if a.ip != ev.IP {
+		return false
+	}
+	return true
+}
+
+func (a approxTime) String() string {
+	now := time.Now().UTC()
+	event := kcp.Event{VisitedAt: now, Day: now.Weekday().String(), IP: a.ip}
+	return fmt.Sprintf("%v, with deviation of %v", event, a.dev)
 }
