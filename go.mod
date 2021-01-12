73298fe2b5bb4c2abe3494f398e79a88d83fe70f
sarunas <sbucius@exadel.com>
2021-01-12 16:18:14 +0200

    add upload-image route to mux
    functions purpose is to practise working with files, http

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/rs/xid v1.2.1
 	github.com/ugorji/go v1.2.2 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
--- platform/services/mux.go
+++ platform/services/mux.go
@@ -2,10 +2,14 @@ package services
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/xid"
 )
 
 // SetRoutes sets routes for http.ListenAndServe.
@@ -14,7 +18,65 @@ func SetRoutes(h Handler) *mux.Router {
 	r.HandleFunc("/api/visits", postVisitHandler(h)).Methods("POST")
 	r.HandleFunc("/api/visits", getVisitsHandler(h)).Methods("GET")
 	r.HandleFunc("/api/visits/{ip}", getVisitsByIPHandler(h)).Methods("GET")
+	r.HandleFunc("/api/upload-image", uploadImageHandler).Methods("POST")
 	return r
+}
+
+// Set max file size to 5MB.
+const maxFileSize = 1024 * 1024 * 5
+
+func uploadImageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.ContentLength > maxFileSize {
+		http.Error(w, "file size too big", http.StatusBadRequest)
+		return
+	}
+	r.Body = http.MaxBytesReader(w, r.Body, maxFileSize)
+
+	f, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "unexpected error occured", http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		http.Error(w, "unexpected error occured", http.StatusInternalServerError)
+		return
+	}
+
+	var ext string
+	switch mime := http.DetectContentType(b); mime {
+	case "image/jpeg":
+		ext = "jpeg"
+	case "image/png":
+		ext = "png"
+	default:
+		http.Error(w, "wrong file type", http.StatusBadRequest)
+		return
+	}
+
+	// Could move to SetRoutes() to create and not check every request.
+	// Leaving here to not spread the code.
+	if err := os.MkdirAll("images", os.ModeDir); err != nil {
+		http.Error(w, "unexpected error occured", http.StatusInternalServerError)
+		return
+	}
+
+	fname := fmt.Sprintf("%v.%v", xid.New().String(), ext)
+
+	if err := ioutil.WriteFile("images/"+fname, b, 0666); err != nil {
+		http.Error(w, "unexpected error occured", http.StatusInternalServerError)
+		return
+	}
+
+	resp := map[string]string{"filename": fname}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "unexpected error occured", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func postVisitHandler(h Handler) func(w http.ResponseWriter, r *http.Request) {
